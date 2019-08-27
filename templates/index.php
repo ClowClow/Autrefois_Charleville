3a522eb39090d5a1a29ed0a2941926b0c302b06a
ClowClow <c.servieres@simplon-charleville.fr>
2019-08-27 23:09:57 +0200

    début de mise en place navbar et description + bouton jouer

---
+++ templates/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Autrefois Charleville</title>
+
+  <link href="../public/styles/css/reset.css" type="text/css" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://bootswatch.com/4/materia/bootstrap.min.css">
+  <link href="../public/styles/css/index.css" type="text/css" rel="stylesheet"/>
+</head>
+
+<body>
+  <!-- NAVBAR-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Autrefois Charleville</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">BDD</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Infos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Connexion</a>
+      </li>
+    </ul>
+    <!--<form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Rechercher">
+      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+    </form>-->
+  </div>
+</nav>
+<!-- FIN NAVBAR-->
+
+<!-- SLIDER -->
+<section class="slider">
+</section>
+<!-- FIN SLIDER-->
+
+<!-- PRESENTATION-->
+<center>
+<section class="card border-primary mb-5" style="max-width: 40rem;">
+  <div class="card-header">Et si vous pouviez remonter le temps et vivre à
+    Charleville il y a 300 ans ?</div>
+  <div class="card-body">
+    <p class="card-text">Imaginez-vous dans la peau d'un carolomacérien du XVIIème
+    siècle pour organiser un baptême ou un mariage selon les traditions de l'époque.
+    Découvrez au fil de vos recherches les us et les coutumes de ceux qui ont contribué
+    à créer l'histoire de la ville créée par Charles de Gonzague. Quels étaient les
+    personnages invités à la cérémonie, dans quels quartiers vivaient-ils, quels
+    étaient leurs métiers, pour certains oubliés, et quels quartiers d'alors existent
+    encore aujourd'hui ? Venez jouer avec nous pour plonger dans le Charleville d'autrefois!
+    </p>
+  </div>
+</section>
+<button type="button" class="btn mb-5">Jouer !</button>
+</center>
+<!-- FIN PRESENTATION-->
+
+  <footer>
+  </footer>
+</body>
+
+</html>
