e925f454bb5d415c273f01a71a3bc8f654e8c6de
MoiAngie <a.maas@simplon-charleville.fr>
2019-08-28 17:37:58 +0200

    collapse sur les infos

--- templates/infos.php
+++ templates/infos.php
@@ -56,6 +56,16 @@
           le dimanche suivant. En la matière les historiens locaux nous invitent
           par ailleurs ici à faire une double lecture de la chanson « La claire fontaine »…
         </p>
+        <span class="collapsible"></span>
+
+        <div class="art-content">
+
+          <p class="card-text">
+            -La demande en mariage : Souvent le prétendant a pu sonder le terrain mais la demande officielle est faite par les parents du marié à ceux de la mariée. C’est « l’coup d’chapiau » où les questions de dot et d’intérêts sont âprement discutées. Aimer ne suffit pas, il faut le prouver : achat de bijoux, châles, d’assiettes au nom des époux… Les préparatifs se font sur plusieurs jours. Dès le jeudi on procède au nettoyage et à la décoration du local choisi. Le samedi, les noceilleux sont en palada (en grande cérémonie), on va chercher la jeune fille chez elle en cortège. Une chanson peut être entonnée : « Ouvrez-moi donc la porte ma mie si vous m’aimez ». Après quelques pourparlers la porte est ouverte le fiancé cherche sa belle, la trouve, la saisit et scelle d’un baiser sa prise de possession. Cette cérémonie accomplie, le cortège s’achemine vers la mairie (à partir du XIXe siècle) et l’église. Tout le village est là et pour se distinguer les invités portent un ruban de soie. Le cortège s’ébranle conduit par le « violoneux » suivi de la fiancée au bras de son père, le promis fermant la marche au bras de sa mère.
+          </p>
+          <p class="card-text">
+            -Le jour des noces : Lors de la remise des alliances, l’époux doit introduire l’anneau jusqu’à la troisième phalange s’il veut acquérir la maîtrise du foyer ; si la femme plie le doigt, elle « portera la culotte ». Le repas est essentiel : « les invités ont plus d’appétit que de dévotion. Il a souvent lieu dans une grange préparée pour l’occasion. Les vieux sont à droite, les jeunes à gauche, les mariés au centre. Au milieu du repas on apporte un colis à la mariée : poupon rose en celluloïd ou autre symbole de vie (des petits oiseaux qu’elle remettait en liberté). Vers la fin du repas, un des garçons d’honneur se glisse discrètement sous la table pour enlever la jarretière de l’épouse. »
+          </p>
       </div>
     </div>
   </section>
@@ -76,6 +86,12 @@
         pour les mégir. Le mégissier se charge des petites peaux : ovins, caprins,
         porcins. Tandis que le tanneur s'occupe des grandes peaux : bovins et équidés.
         </p>
+        <span class="collapsible"></span>
+
+        <div class="art-content">
+          <p class="card-text bold">Petit lexique</p>
+          <p class="card-text bold">L’alun : sels isomorphes.</p>
+          <p class="card-text bold">Mégir (verbe) : tanner une peau en utilisant l’alun.</p>
       </div>
     </div>
   </section>
@@ -97,7 +113,7 @@
           puis d’un repas du soir. Ces deux repas sont composés de multiples plats,
           en particulier de viandes différentes.
         </p>
-      </div>
+
     </div>
   </section>
   <!-- FIN PARTIE CEREMONIE -->
@@ -117,8 +133,16 @@
           les registres paroissiaux, on la trouve assez souvent au XVIIe siècle
           pour pouvoir en faire une analyse. L’horaire de la cérémonie religieuse
           est en effet un indicateur précieux : il détermine le déroulement des
-          festivités
+          festivités...
         </p>
+        <span class="collapsible"></span>
+
+        <div class="art-content">
+          <p class="card-text">L’heure « indue » du mariage (...) en 1644 est une heure nocturne, car à partir de cette date, toutes les indications données par les registres paroissiaux sont postérieures au coucher du soleil . Les horaires se distribuent entre 7 heures du soir et 1 heure du matin en hiver, et entre 10 heures et 1 heure les soirs d’été . La moyenne est à 9 heures et demie en hiver et 11 heures en été, soit dans les deux cas quatre heures après le coucher du soleil.</p>
+
+          <p class="card-text">Cette mode du mariage en soirée se répand progressivement. La proportion des mariages avec dispense d’heure passe de 2 % dans la décennie 1640-1649 à 8 % en 1670-1679. Comme dans les exemples cités, elle est souvent (dans un quart des cas) associée à une dispense de lieu. C’est bien l’insertion des fêtes familiales dans le temps et l’espace citadin que l’on voit changer.</p>
+
+      </div>
       </div>
     </div>
   </section>
@@ -127,6 +151,8 @@
   <!-- FOOTER -->
   <?php include "footer.php"; ?>
   <!-- FIN FOOTER -->
+
+  <script src="../public/js/script.js"></script>
 </body>
 
 </html>
